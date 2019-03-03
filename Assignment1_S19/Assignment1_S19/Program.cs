b00712f7ce3ca070eadef03b36b0e452f976c521
Quy <quy.h.le@gmail.com>
2019-03-03 13:07:00 -0500

    Added shell application

---
+++ Assignment1_S19/Assignment1_S19/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Assignment1_S19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 15;
+            printPrimeNumbers(a, b);
+
+            int n1 = 5;
+            double r1 = getSeriesResult(n1);
+            Console.WriteLine("The sum of the series is: " + r1);
+
+
+            int n4 = 5;
+            printTriangle(n4);
+
+            int[] arr = new int[] { 1, 2, 3, 2, 2, 1, 3, 2 };
+            computeFrequency(arr);
+
+            // write your self-reflection here as a comment
+        }
+
+        public static void printPrimeNumbers(int x, int y)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printPrimeNumbers()");
+            }
+        }
+
+        public static double getSeriesResult(int n)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing getSeriesResult()");
+            }
+
+            return 0;
+        }
+
+        public static void printTriangle(int n)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printTriangle()");
+            }
+        }
+
+        public static void computeFrequency(int[] a)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing computeFrequency()");
+            }
+        }
+
+    }
+}
---
+++ Assignment1_S19/Assignment1_S19_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment1_S19_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
